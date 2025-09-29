3694134294fa8d21f03df12bdb75e9e1e24b4027
Filya <s-feloniuk@ukr.net>
2025-09-30 00:04:38 +0300

    фикс

--- src/lib/getAllPlayers.jsx
+++ src/lib/getAllPlayers.jsx
@@ -1,6 +1,7 @@
 export default async function getAllPlayers(page = 1, orderBy = "lastDealDate") {
   const url = new URL('/api/players', process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000');
-  
+  const apiKey = process.env.JAVES_API_KEY || process.env.NEXT_PUBLIC_JAVES_API_KEY;
+
   url.searchParams.append('page', page);
   url.searchParams.append('orderBy', orderBy);
 
@@ -9,6 +10,7 @@ export default async function getAllPlayers(page = 1, orderBy = "lastDealDate") 
     headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
+      'x-api-key': apiKey
     },
     // Добавь кеш для оптимизации
     next: { revalidate: 600 } // кеш на 600 секунд
