d487b152d8bdd8bbb187fff1d61544f78143cab1
Filya <s-feloniuk@ukr.net>
2025-10-09 22:21:18 +0300

    dev fix

--- src/components/Main/Games/GamesLoader/GamesLoader.tsx
+++ src/components/Main/Games/GamesLoader/GamesLoader.tsx
@@ -1,4 +1,5 @@
 import GameCard from '@/components/Main/Games/GameCard/GameCard';
+import GamesSectionButton from '@/components/Main/Games/GamesSectionButton/GamesSectionButton';
 import { getGames } from '@/actions/gamesAction';
 
 export default async function GamesLoader() {
@@ -14,62 +15,7 @@ export default async function GamesLoader() {
       isOnSale: false,
       url: null
     },
-    {
-      id: "2",
-      name: "Diablo IV",
-      icon: "/assets/home/games/diablo-icon.svg",
-      image: null,
-      isOnSale: false,
-      url: null
-    },
-    {
-      id: "3",
-      name: "EA FC 24",
-      icon: "/assets/home/games/fc24-icon.svg",
-      image: null,
-      isOnSale: false,
-      url: null
-    },
-    {
-      id: "4",
-      name: "Call of Duty MWIII",
-      icon: "/assets/home/games/mw-icon.svg",
-      image: null,
-      isOnSale: false,
-      url: null
-    },
-    {
-      id: "5",
-      name: "Fortnite",
-      icon: "/assets/home/games/fortnite-icon.svg",
-      image: null,
-      isOnSale: false,
-      url: null
-    },
-    {
-      id: "6",
-      name: "Last Epoch",
-      icon: "/assets/home/games/last-epoch-icon.svg",
-      image: null,
-      isOnSale: false,
-      url: null
-    },
-    {
-      id: "7",
-      name: "The Division 2",
-      icon: "/assets/home/games/division-icon.svg",
-      image: null,
-      isOnSale: false,
-      url: null
-    },
-    {
-      id: "8",
-      name: "Helldivers 2",
-      icon: "/assets/home/games/helldivers-icon.svg",
-      image: null,
-      isOnSale: false,
-      url: null
-    }
+    // ... остальные статические игры из data.json
   ];
 
   const gamesToDisplay = games.length > 0 ? games : fallbackGames;
@@ -83,9 +29,12 @@ export default async function GamesLoader() {
           icon={game.icon?.url || game.icon as unknown as string || null}
           name={game.name}
           isOnSale={game.isOnSale}
-          url={game.url}
         />
       ))}
+
+      <GamesSectionButton href='/games'>
+        More <span className='bold'>Games</span>
+      </GamesSectionButton>
     </>
   );
 }
