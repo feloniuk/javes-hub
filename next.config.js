91701f08d693e0482b5a3143825467848a3ee004
Filya <s-feloniuk@ukr.net>
2025-11-16 11:43:01 +0200

    next fix

--- next.config.js
+++ next.config.js
@@ -14,6 +14,12 @@ const nextConfig = {
           port: '',
           pathname: '/media/**',
         },
+        {
+          protocol: 'https',
+          hostname: 'mmonster.co',
+          port: '',
+          pathname: '/media/**',
+        },
       ],
     },
   }
